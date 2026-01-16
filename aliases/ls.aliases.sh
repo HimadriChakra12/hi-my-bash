77cbc86130de26252a075c29d0869161c5e2e3f1
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-01-16 19:49:44 +0000

    Much Better ls alias

--- aliases/ls.aliases.sh
+++ aliases/ls.aliases.sh
@@ -14,14 +14,20 @@ if command -v eza &> /dev/null; then
   alias lm='ls -al | more'        # pipe through 'more'
   alias lo='ls -laSFh'            # sort by size largest to smallest
   alias lr='ls -lR'               # recursive ls
+else
+  echo "Install eza for betterment"
+  alias ls='ls -lh --group-directories-first'
+  alias lsa='ls -a'
+  alias lta='lt -a'
+  alias dir='ls -hFx'
+  alias l='ls -lathF'             # long, sort by newest to oldest
+  alias L='ls -latrhF'            # long, sort by oldest to newest
+  alias lc='ls -lcr'              # sort by change time
+  alias lm='ls -al | more'        # pipe through 'more'
+  alias lo='ls -laSFh'            # sort by size largest to smallest
+  alias lr='ls -lR'               # recursive ls
 fi
 
-# Directory Listing aliases
-
-
-#   lr:  Full Recursive Directory Listing
-#   ------------------------------------------
 alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
-
 alias dud='du -d 1 -h'                      # Short and human-readable directory listing
 alias duf='du -sh *'                        # Short and human-readable file listing
