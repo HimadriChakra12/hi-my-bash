4f4e43e14b8724ce7d005dacb29839f83bc941b1
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-01-17 14:10:57 +0000

    Added my alias

---
+++ aliases/him.aliases.sh
@@ -0,0 +1,12 @@
+alias exp="nvim $PWD"
+alias Exp="pcmanfm $PWD & disown"
+
+alias ep="$EDITOR ~/.bashrc"
+alias so="source $HOME/.bashrc"
+alias v="nvim"
+alias nano="nvim"
+alias q="exit"
+alias c="clear"
+alias gg="nvim -c Git"
+
+alias fst="fastfetch"
